b7dd099003bb7f44037e21f0e1e2dbde0784d9b5
Danira Cortez <cortezd334@gmail.com>
2020-05-15 19:09:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
   # Print a numbered list of each element
   
 def list_dwarves(array)
-  array.each_with_index do |characters|
-    puts characters
+  array.each_with_index do |characters, index|
+    puts index + 1 characters
   end
 end
